12b35ea15979ab79f1b3a4f395338e3b7436c8d3
Mimi Hoang <miminhoang1@gmail.com>
2019-09-25 17:53:29 -0400

    commit for pull

--- data/migrations/20190924165253_bootstrap.js
+++ data/migrations/20190924165253_bootstrap.js
@@ -3,14 +3,37 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('users', tbl => {
         tbl.increments()
-        tbl.text('username', 32)
+        tbl.string('username', 32)
             .unique()
             .notNullable()
-        tbl.text('password', 64)
+        tbl.string('password', 64)
             .notNullable()
+    })
+    .createTable('expriences', tbl => {
+      tbl.increments()
+      tbl.integer('origin_user')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('users') 
+      tbl.integer('rateing')
+        .notNullable()
+        .unsigned()
+      tbl.string('name', 128)
+        .notNullable()
+        .unique()
+      tbl.string('location', 128)
+        .notNullable()
+      tbl.string('priceing', 32)
+        .notNullable()
+      tbl.string('description', 512)
+        .notNullable()
+      tbl.string('img_url', 512)
+        .notNullable()
     })
 };
 
 exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('expriences')
   return knex.schema.dropTableIfExists('users')
 };
