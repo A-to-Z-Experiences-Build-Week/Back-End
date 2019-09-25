ad3957ec2337d1dd051b3154dcc156cc1730b05d
Mimi Hoang <miminhoang1@gmail.com>
2019-09-25 12:49:18 -0400

    Created signup endpoint

--- users/users-router.js
+++ users/users-router.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
-
+const Users = require('./users-model')
 router.get("/", (req, res) => {
     res.send('i am of the existing')
 })
+router.post("/signup", (req, res) => {
+    const {username, password} = req.body
 
+    if(!username || !password) {
+        res.status(400).json({error: "Sign up requires a username & password"})
+    }
+    else{
+        Users.add(req.body)
+        .then(user => {
+            res.status(201).json(user)
+            
+        })
+        .catch(error => {
+            res.status(500).json({error: "There was an issue adding new user to database"})
+        })
+    }
+})
 module.exports = router
