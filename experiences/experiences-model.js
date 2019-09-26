b03be8993bafbd176a85a7de16e43065bd455c13
Bishop <lake.d.bishop@gmail.com>
2019-09-26 08:26:53 -0500

    find by id get now complete

--- experiences/experiences-model.js
+++ experiences/experiences-model.js
@@ -1,10 +1,20 @@
 const data = require('../data/db-config')
 
 module.exports = {
-    add
+    add,
+    findBy,
+    find
 }
 
 
 async function add(exp) {
     const [id] = await data('experiences').insert(exp)
+}
+
+function findBy(exp) {
+    return data('experiences').where('id', exp)
+}
+
+function find(exp) {
+    return data('experiences')
 }
--- experiences/experiences-router.js
+++ experiences/experiences-router.js
@@ -1,7 +1,32 @@
 const router = require('express').Router();
 const Experiences = require('./experiences-model')
-router.get("/", (req, res) => {
+
+router.get("/sanity", (req, res) => {
     res.send('i am of the existing')
+})
+
+router.get("/", (req, res) => {
+    Experiences.find()
+        .then(list => {
+            res.status(200).json(list)
+        })
+        .catch(error => {
+            res.status(500).json({error: 'there was an issue retriveing the data from the server'})
+        })     
+})
+
+router.get("/:id", (req, res) => {
+    if (!req.params.id) {
+        res.status(401).json({error: "you must give an id to find by id"})
+    } else {
+        Experiences.findBy(req.params.id)
+            .then(list => {
+                res.status(200).json(list)
+            })
+            .catch(error => {
+                res.status(500).json(error)
+            })
+    }
 })
 
 router.post("/newexp", (req, res) =>{
