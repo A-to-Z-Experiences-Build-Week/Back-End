1da7bd626ada722d0f5469e6f1a1652d0aa806b9
Bishop <lake.d.bishop@gmail.com>
2019-09-26 16:42:25 -0500

    test to fix issue

--- api/server.js
+++ api/server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const usersrouter = require('../users/users-router')
 const experiencesRouter = require('../experiences/experiences-router')
 const authenticate = require('../auth/auth-middleware')
+const helmet = require("helmet");
+const cors = require("cors");
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(cors());
 server.use('/api/users', usersrouter)
 server.use('/api/exp', authenticate, experiencesRouter)
 
